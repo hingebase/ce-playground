10e5cd0cef8ed7bbec8e6433fa528c9507b73e6d
hingebase <zcliu@pku.edu.cn>
2026-01-18 10:16:43 +0800

    🚨 Fix Ruff warning `RUF067`

--- _ce_playground/__main__.py
+++ _ce_playground/__main__.py
@@ -12,7 +12,49 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import main
+__all__ = ["main"]
+
+import asyncio
+import json
+import sys
+from typing import TYPE_CHECKING
+
+from . import _c, _common, _cpp, _numba, _unpack
+
+if TYPE_CHECKING:
+    from collections.abc import Mapping
+
+
+def main() -> None:
+    _unpack.main()
+    info, tasks = asyncio.run(_envs())
+    envs = {k: json.loads(v.result()) for k, v in tasks.items()}
+    context = _c.local_properties(envs)
+    _cpp.local_properties(context, envs)
+    _numba.local_properties(json.loads(info), context["demangler"])
+
+
+async def _envs() -> tuple[bytes, Mapping[str, asyncio.Task[bytes]]]:
+    tasks: dict[str, asyncio.Task[bytes]] = {}
+    async with asyncio.TaskGroup() as g:
+        info = g.create_task(_common.pixi("info", "--json"))
+        tasks["clang"] = g.create_task(_inspect("clang"))
+        if sys.platform != "darwin":
+            tasks["intel"] = g.create_task(_inspect("intel"))
+            if sys.platform == "win32":
+                tasks["gcc"] = g.create_task(_inspect("gcc", "_inspect2"))
+                tasks["msvc"] = g.create_task(_inspect("msvc"))
+            else:
+                tasks["gcc"] = g.create_task(_inspect("gcc"))
+    return info.result(), tasks
+
+
+async def _inspect(env: str, task: str = "_inspect") -> bytes:
+    out = await _common.pixi("r", "-e", env, task)
+    if task == "_inspect2":
+        out = out[out.index(b'{"'):]
+    return out
+
 
 if __name__ == "__main__":
     main()
